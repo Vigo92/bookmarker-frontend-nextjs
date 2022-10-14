ea471769053d1fa83ba9dc03ce9ea09e047bd255
Vigo92 <obiaugochukwu@gmail.com>
2022-10-14 19:50:34 +0100

    Next js bookmarker-api

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Ugo is on Point!!!!</h1>
+      <button className='btn btn-primary'>Click</button>
+    </div>
+  )
+}
+
+export default Home
